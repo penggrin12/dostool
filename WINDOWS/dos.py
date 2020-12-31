7dc05aa23484a84116a599305e0de629c5af60ed
penggrin12 <76795163+penggrin12@users.noreply.github.com>
2020-12-31 17:46:37 +0300

    build2

--- WINDOWS/dos.py
+++ WINDOWS/dos.py
@@ -1,7 +1,28 @@
 import sys,os
-os.system('cls')
+
+def info():
+        print('#      DOS TOOL')
+        print('#       Build 2')
+        print('#     By Penggrin \n')
+        
+def cls():
+        os.system('cls')
+        
+cls()
+info()
+
 print('# Выбери сайт:')
 site = input('# >')
-os.system('cls')
-os.system('python modules/dos.py '+site)
 
+if site == "":
+        print('# Сайт не может быть пустым')
+        cls()
+        exit()
+        
+elif site == " ":
+        print('# Сайт не может быть пустым')
+        cls()
+        exit()
+        
+os.system('clear')
+os.system('python modules/dos.py '+site)
--- dos.py
+++ dos.py
@@ -1,7 +1,29 @@
 import sys,os
-os.system('clear')
+
+def info():
+        print('#      DOS TOOL')
+        print('#       Build 2')
+        print('#     By Penggrin \n')
+        
+def cls():
+        os.system('clear')
+        
+cls()
+info()
+
 print('# Выбери сайт:')
 site = input('# >')
+
+if site == "":
+        print('# Сайт не может быть пустым')
+        cls()
+        exit()
+        
+elif site == " ":
+        print('# Сайт не может быть пустым')
+        cls()
+        exit()
+        
 os.system('clear')
 os.system('python modules/dos.py '+site)
 
